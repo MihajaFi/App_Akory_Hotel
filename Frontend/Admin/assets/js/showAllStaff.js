54eb19f5d9f6ec8b813716fa3a191204b93e160b
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-02 12:25:28 +0300

    feat: add showAllStaff.css file

---
+++ Frontend/Admin/assets/js/showAllStaff.js
@@ -0,0 +1,28 @@
+function showAllStaff(data) {
+    const body_table = document.querySelector('#table_body');
+
+    data.map((values)=> {
+        let dataInitial = "";
+        dataInitial += `<tr>
+      <td>${values.id_employee}</td>
+      <td>${values.first_name}</td>
+      <td>${values.last_name}</td>
+      <td>${values.email}</td>
+      <td>${values.work_contact}</td>
+      <td>${values.country}</td>
+    </tr>`;
+    body_table.innerHTML += dataInitial;
+    });
+}
+function addAllinformation() {
+    fetch("http://localhost:5000/staff")
+    .then((data) => {
+      return data.json(); //convert to object
+    })
+    .then((data) => {
+      showAllStaff(data);
+      
+    })
+    .catch(data=>alert(data))
+}
+addAllinformation() ;
