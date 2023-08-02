9adaeb1b6b8abfc0d43827dc9b9ff5112aeca575
Fifaliana <hei.fifaliana@gmail.com>
2023-08-02 09:23:49 +0300

    feat : server of the App Akory

---
+++ Backend/src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { router } from "./router.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+import session from "express-session"; // Correction de la faute de frappe ici
+
+const app = express();
+
+// Configure le moteur de rendu EJS
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+
+
+app.listen(5000, () => {
+  console.log("Serveur démarré (http://localhost:5000/) !");
+});
