148c01c44761c69bfcaadb7638beb2214bf603dc
Fifaliana <hei.fifaliana@gmail.com>
2023-08-03 13:28:50 +0300

    feat : information of room

--- Backend/src/Queries/AllBasic.js
+++ Backend/src/Queries/AllBasic.js
@@ -22,12 +22,23 @@ const getAllReservation = `
         select reservation.id_reservation , client.name , client.email ,reservation.date_arrived ,reservation.number_of_person,reservation.leaving_date ,client.CIN
         from reservation 
         inner join client ON reservation.id_client = client.id_client ;
+` ;
+
+const getDetailRoomOccupiedByClient = `
+        SELECT room.id_room, room.room_number, room.room_type, room.capacity_room, room_features.*
+        FROM room
+        INNER JOIN reservation ON room.id_reservation = reservation.id_reservation
+        INNER JOIN client ON reservation.id_client = client.id_client
+        INNER JOIN room_features ON room.id_features = room_features.id_features
+        WHERE client.id_client = 9
+        AND reservation.leaving_date >= CURRENT_DATE;
 `
 
 const AllBasic = {
     getAllRecptionist,
     getAllReservation,
     getClientNotPaid,
+    getDetailRoomOccupiedByClient,
 };
 
 export default AllBasic;
