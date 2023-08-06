30580b51b43e7ec5c33f514bae86435eac8c63b1
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-06 23:32:01 +0300

    feat: create home.js file

---
+++ Frontend/assets/js/home.js
@@ -0,0 +1,45 @@
+const bookbox = document.getElementById("guestdetailpanel");
+const suite = document.getElementById("InfoRoomSuite");
+const deluxe = document.getElementById("InfoRoomdeluxe");
+const guest = document.getElementById("InfoRoomguest");
+const single = document.getElementById("InfoRoomsingle");
+
+openbookbox = () =>{
+bookbox.style.display = "flex";
+}
+
+closebox = () =>{
+bookbox.style.display = "none";
+}
+
+openinfosuit = () =>{
+suite.style.display = "flex";
+}
+
+closeboxsuit = () =>{
+suite.style.display = "none";
+}
+
+openroomdeluxe = () =>{
+deluxe.style.display = "flex";
+}
+
+closeboxdeluxe = () =>{
+deluxe.style.display = "none";
+}
+
+openroomguest = () =>{
+guest.style.display = "flex";
+}
+
+closeboxguest = () =>{
+guest.style.display = "none";
+}
+
+openroomsingle = () =>{
+single.style.display = "flex";
+}
+
+closeboxsingle = () =>{
+single.style.display = "none";
+}
