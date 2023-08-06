06ff4fc5dab79170f193eb7dc1ae877308d32015
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-06 23:30:20 +0300

    feat: update showAllClient.js

--- Frontend/Admin/assets/js/showAllClient.js
+++ Frontend/Admin/assets/js/showAllClient.js
@@ -52,4 +52,35 @@ const searchFun = () => {
           }
       }
   }
-}   
+}  
+
+
+function deleteAction(id) {
+    // Demande de confirmation à l'utilisateur avant de supprimer le client
+    const confirmation = confirm("Êtes-vous sûr de vouloir supprimer cet client ?");
+    
+    // Si l'utilisateur confirme la suppression
+    if (confirmation) {
+      // Endpoint de l'API pour supprimer le client
+      const deleteURL = `http://localhost:5000/client/${id}`;
+  
+      // Configuration de la requête fetch pour envoyer une requête DELETE au serveur
+      fetch(deleteURL, {
+        method: "DELETE",
+      })
+      .then((response) => {
+        // Vérification de la réponse du serveur
+        if (!response.ok) {
+          throw new Error("Erreur lors de la suppression de ce client");
+        }
+        // Suppression réussie, recharger la page pour mettre à jour le tableau
+        location.reload();
+      })
+      .catch((error) => {
+        console.error("Erreur :", error);
+        // Gérer les erreurs d'une manière appropriée, par exemple, afficher un message d'erreur à l'utilisateur
+        alert("Une erreur est survenue lors de la suppression de ce client.");
+      });
+    }
+  }
+  
