7d1002a98c024dc95a717655f4b5b1b6bcc6f373
Fitahi <hei.tsiaroniaina@gmail.com>
2023-08-08 00:11:13 +0300

    feat : Endpoint show total reservation

--- Backend/src/router.js
+++ Backend/src/router.js
@@ -672,3 +672,16 @@ router.get('/totalAmountStatus',(req,res)=>{
     res.send(data.rows);
   });
 })
+
+// Total reservation
+router.get('/dashboard', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT COUNT(*) as total_booked_room FROM reservation');
+    const totalBookedRoom = result.rows[0].total_booked_room;
+    res.send(totalBookedRoom.toString());
+  } catch (err) {
+    console.error('Error retrieving total booked room:', err);
+    res.status(500).send('Error retrieving total booked room');
+  }
+});
+
